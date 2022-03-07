598ad9eaba2f41dda350ef187a61f6f0562f8010
MihirJoshiB <98804755+MihirJoshiB@users.noreply.github.com>
2022-03-07 22:45:58 +0530

    api

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const app = express();
+const port = process.env.port || 3001;
+
+app.get("/", (req,res) => {
+    res.send("Hello UTU Bmiit from barodli");
+})
+app.post("/gym",(req , res) => {
+    res.send("Hello UTU");
+})
+
+app.listen(port, () => {
+    console.log(`connection is setup at ${port}`);
+})
